e4ca0f7132ec8ea117164a46920450b770bb439c
okochi-mika <hiyoko_m@icloud.com>
2025-06-18 15:57:34 +0900

    Initial commit

---
+++ app/Models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
+class Product extends Model
+{
+    use HasFactory, Sortable;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+        'image',
+        'recommend_flag',
+        'carriage_flag',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+    
+    public function favorited_users() {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
+    public function getAverageScoreAttribute()
+    {
+        return round($this->reviews()->avg('score') ?? 0, 1);
+    }
+}
